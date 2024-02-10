8788da803aa0c50b71a91e490d60511142b38799
unknown <u20232216799@usco.edu.co>
2024-02-10 13:11:54 -0500

    primer HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
  */
-
+package HelloWorld; //linea 
 /**
  *
  * @author acer
@@ -13,6 +13,8 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        // TODO code application logic here
+        System.out.println("Hello World");
+        
     }
 }
+
